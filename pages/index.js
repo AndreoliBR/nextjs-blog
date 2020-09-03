e160160ddd597679aa59db42aba4be743a37982d
AndreoliBR <andremarcia74@hotmail.com>
2020-09-02 23:26:20 -0400

    index changes

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        Gosto de codar
+        Vamos fingir q essa bio é super interessante
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
