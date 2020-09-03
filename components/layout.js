a1ff143bba8207cdf2ccca9758a3699958b64b72
AndreoliBR <andremarcia74@hotmail.com>
2020-09-03 00:38:39 -0400

    minor changes

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = '<andrrms />'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "andrrms' blog"
 
 export default function Layout({ children, home }) {
   return (
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="code blog built by andrrms using Next.js"
         />
         <meta
           property="og:image"
